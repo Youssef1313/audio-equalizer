797bc033a2b91c7f2c97637111a808cea16a15c3
Youssef1313 <youssefvictor00@gmail.com>
2021-06-09 21:49:43 +0200

    Read sound file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+import soundfile as sf
 from tkinter import filedialog
 
 
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
+
+data, fs = sf.read(file_path)
